b2d66678e22277a192b5c2b97ffdce665e855588
clovis-junior <clovis.sa.junior@gmail.com>
2026-06-08 21:16:48 -0300

    modified:   interactions/admin/status.js

--- interactions/admin/status.js
+++ interactions/admin/status.js
@@ -1,63 +1,82 @@
-const { SlashCommandBuilder, MessageFlags, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, ActivityType, MessageFlags, PermissionsBitField } = require('discord.js');
 
-const status = [
-   { name: 'Online', value: 'online'}, 
-   { name: 'Ausente', value: 'idle'},
-   { name: 'Não Perturbe', value: 'dnd'},
-   { name: 'Invisivel', value: 'invisible'}
+const statusOptions = [
+   { name: 'Online', value: 'online' }, 
+   { name: 'Ausente', value: 'idle' },
+   { name: 'Não Perturbe', value: 'dnd' },
+   { name: 'Invisivel', value: 'invisible' }
 ];
 
-const activities = [
-   { name: 'Jogando', value: 'PLAYING'},
-   { name: 'Transmitindo', value: 'STREAMING'},
-   { name: 'Ouvindo', value: 'LISTENING'},
-   { name: 'Assistindo', value: 'WATCHING'}
+const activitiesOptions = [
+   { name: 'Jogando', value: ActivityType.Playing },
+   { name: 'Transmitindo', value: ActivityType.Streaming },
+   { name: 'Ouvindo', value: ActivityType.Listening },
+   { name: 'Assistindo', value: ActivityType.Watching }
 ];
+
+function urlCheck(value) {
+  let filter = /^(https?)\:\/\/(?:www\.)?([a-zA-Z0-9-:]{1,256})\.([a-zA-Z0-9.]{2,})\b([a-zA-Z0-9-_()@:%\+.~#?&\/\/=]*)$/is;
+  return (filter.test(value)) ? true : false;
+}
 
 module.exports = {
     name: 'status',
     data: {
         description: 'Define o status do Alfredo',
+        permissions: [PermissionsBitField.Flags.Administrator],
         options: [{
             type: 3,
             name: 'status',
             description: 'Status',
             required: true,
-            choices: status
+            choices: statusOptions
+        },{
+            type: 4,
+            name: 'atividade',
+            description: 'Atividade',
+            required: false,
+            choices: activitiesOptions
         },{
             type: 3,
-            name: 'activity',
-            description: 'Atividade',
-            required: true,
-            choices: activities
-        },{
-            type: 6,
-            name: 'description',
+            name: 'detalhe',
             description: 'Detalhe',
-            required: true
+            required: false
         }]
     },
 
     async execute(client, interaction) {
         const status = interaction.options.getString('status');
-        const activity = interaction.options.getString('player');
-        const description = interaction.options.getString('description');
+        const activity = interaction.options.getInteger('atividade');
+        const description = interaction.options.getString('detalhe');
 
-        await interaction.deferReply();
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
         try {
-            client.user.setActivity(description, { type: activity });
-            client.user.setStatus(status); 
+            const presence = { status: status };
+
+            if (description) {
+                activitiesObj = {
+                    name: description,
+                    type: activity ?? ActivityType.Playing
+                };
+                
+                if (activity === ActivityType.Streaming)
+                    activitiesObj.activities.url = urlCheck(description) ? description : 'https://twitch.tv';
+
+                presence.activities = [activitiesObj]
+            }
+
+            client.user.setPresence(presence);
 
             await interaction.editReply({
-                content: 'Meu status foi alterado!',
+                content: '✅ Meu status foi alterado!',
                 flags: MessageFlags.Ephemeral
             })
         } catch (err) {
             console.error(err);
 
             await interaction.editReply({
-                content: '❌ Ocorreu um erro ao excluir as mensagens.',
+                content: '❌ Ocorreu um erro ao alterar o status.',
                 flags: MessageFlags.Ephemeral
             })
         }
